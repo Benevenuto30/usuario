e9be6bf1471a12972e7c83aefa2f2240ab3a3ae3
Benevenuto30 <rafaelmaster03@gmail.com>
2025-04-06 22:06:33 -0300

    inclusao de metodo put de usuario e metodo get por nome

--- src/main/java/com/benevenuto/usuario/business/UsuarioService.java
+++ src/main/java/com/benevenuto/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.benevenuto.usuario.infrastructure.entity.Usuario;
 import com.benevenuto.usuario.infrastructure.exceptions.ConflictException;
 import com.benevenuto.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.benevenuto.usuario.infrastructure.repository.UsuarioRepository;
+import com.benevenuto.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -52,5 +54,13 @@ public class UsuarioService {
     }
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        String email = jwtUtil.extractUsername(token.substring(7));
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()): null);
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("Email não localizado"));
+        Usuario usuario = usuarioConverter.updateDeUsuario(dto, usuarioEntity);
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
--- src/main/java/com/benevenuto/usuario/infrastructure/security/JwtUtil.java
+++ src/main/java/com/benevenuto/usuario/infrastructure/security/JwtUtil.java
@@ -36,6 +36,7 @@ public class JwtUtil {
                 .getBody(); // Retorna o corpo das claims
     }
 
+
     // Extrai o nome de usuário do token JWT
     public String extractUsername(String token) {
         // Obtém o assunto (nome de usuário) das claims do token
