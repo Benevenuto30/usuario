f52554e8a25e7a3019eaa68ab1d33210162c1ef8
Benevenuto30 <rafaelmaster03@gmail.com>
2025-09-15 09:35:55 -0300

    implementado padrÃao de excecoes

---
+++ src/main/java/com/benevenuto/usuario/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.benevenuto.usuario.controller;
+
+import com.benevenuto.usuario.infrastructure.exceptions.ConflictException;
+import com.benevenuto.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.benevenuto.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(404).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex) {
+        return ResponseEntity.status(409).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(401).body(ex.getMessage());
+    }
+}
